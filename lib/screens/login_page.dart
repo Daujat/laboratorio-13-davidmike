47ad45ebb4de67b15809f6afb0bafd5353bda0e6
vasquezdavidperez <vasquezdavidperez@gmail.com>
2024-07-01 01:06:35 +0000

    lab13-davidmike-v2

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
-
   const LoginScreen({super.key});
 
   @override
@@ -15,12 +14,10 @@ class _LoginScreenState extends State<LoginScreen> {
   late Size mediaSize;
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
-  bool rememberUser =  false;
-
+  bool rememberUser = false;
 
   @override
   Widget build(BuildContext context) {
-
     myColor = Theme.of(context).primaryColor;
     mediaSize = MediaQuery.of(context).size;
 
@@ -28,10 +25,10 @@ class _LoginScreenState extends State<LoginScreen> {
       decoration: BoxDecoration(
         color: myColor,
         image: DecorationImage(
-          image: const AssetImage('assets/arizona_fondo.jpg'),
+          image: const NetworkImage('https://navajotours.com/wp-content/uploads/2019/11/home-quotes-right.jpg'),
           fit: BoxFit.cover,
           colorFilter: ColorFilter.mode(myColor.withOpacity(0.2), BlendMode.darken)
-          ),
+        ),
       ),
       child: Scaffold(
         backgroundColor: Colors.transparent,
@@ -40,11 +37,11 @@ class _LoginScreenState extends State<LoginScreen> {
             Positioned(
               top: 70,
               child: _construirTop()
-              ),
+            ),
             Positioned(
               bottom: 0,
               child: _construirBottom()
-              )
+            )
           ],
         ),
       ),
@@ -53,15 +50,16 @@ class _LoginScreenState extends State<LoginScreen> {
 
   //Creación de una widget externa
   //Parte superior
-  Widget _construirTop(){
+  Widget _construirTop() {
     return SizedBox(
       width: mediaSize.width,
       child: const Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Icon(Icons.location_on_sharp,
-          size: 100,
-          color: Colors.white,
+          Icon(
+            Icons.location_on_sharp,
+            size: 100,
+            color: Colors.white,
           ),
           Text(
             'VER MAPA',
@@ -71,14 +69,14 @@ class _LoginScreenState extends State<LoginScreen> {
               fontSize: 40,
               letterSpacing: 2
             ),
-            )
+          )
         ],
       ),
     );
   }
 
   //Parte inferior
-  Widget _construirBottom(){
+  Widget _construirBottom() {
     return SizedBox(
       width: mediaSize.width,
       child: Card(
@@ -91,13 +89,13 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Padding(
           padding: const EdgeInsets.all(30.0),
           child: _construirFormulario(),
-        ), 
+        ),
       ),
     );
   }
 
   //Formulario
-  Widget _construirFormulario(){
+  Widget _construirFormulario() {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -123,20 +121,20 @@ class _LoginScreenState extends State<LoginScreen> {
         const SizedBox(height: 20),
         _construirOtroLogeo()
       ]
-      );
+    );
   }
 
   //Textos en color plomo para formulario
-  Widget _construirTextoPlomo(String text){
+  Widget _construirTextoPlomo(String text) {
     return Text(
       text,
       style: const TextStyle(
-        color: Colors.grey,        
+        color: Colors.grey,
       ),
-      );
+    );
   }
 
-  Widget _construirInputField(TextEditingController controller, {isPassWord = false}){
+  Widget _construirInputField(TextEditingController controller, {isPassWord = false}) {
     return TextField(
       controller: controller,
       decoration: InputDecoration(
@@ -146,7 +144,7 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 
-  Widget _construirRememberForgot(){
+  Widget _construirRememberForgot() {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
@@ -154,26 +152,26 @@ class _LoginScreenState extends State<LoginScreen> {
           children: [
             Checkbox(
               value: rememberUser,
-              onChanged: (value){
+              onChanged: (value) {
                 setState(() {
                   rememberUser = value!;
                 });
               }
-              ),
+            ),
             _construirTextoPlomo('Recordar contraseña')
           ],
         ),
         TextButton(
-          onPressed: (){},
+          onPressed: () {},
           child: _construirTextoPlomo('Olvide mi contraseña')
-          )
+        )
       ],
     );
   }
 
-  Widget _construirBotonLogeo(){
+  Widget _construirBotonLogeo() {
     return ElevatedButton(
-      onPressed: (){
+      onPressed: () {
         debugPrint("Email: ${emailController.text}");
         debugPrint("Contraseña: ${passwordController.text}");
       },
@@ -188,11 +186,11 @@ class _LoginScreenState extends State<LoginScreen> {
         style: TextStyle(
           color: Colors.orange
         ),
-        )
-      );
+      )
+    );
   }
 
-  Widget _construirOtroLogeo(){
+  Widget _construirOtroLogeo() {
     return Center(
       child: Column(
         children: [
@@ -202,13 +200,13 @@ class _LoginScreenState extends State<LoginScreen> {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Tab(
-                icon: Image.asset('assets/github.png'),                
+                icon: Image.network('https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Github-desktop-logo-symbol.svg/2048px-Github-desktop-logo-symbol.svg.png'),
               ),
               Tab(
-                icon: Image.asset('assets/instagram.png'),
+                icon: Image.network('https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1000px-Instagram_logo_2022.svg.png'),
               ),
               Tab(
-                icon: Image.asset('assets/logotipo-cuadrado-de-vimeo.png'),
+                icon: Image.network('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/64e8d2de-fcbf-4bfb-927d-c7139e92bb58/de7uc5t-a66a0b9f-6385-4cfb-a397-3c719e199adc.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzY0ZThkMmRlLWZjYmYtNGJmYi05MjdkLWM3MTM5ZTkyYmI1OFwvZGU3dWM1dC1hNjZhMGI5Zi02Mzg1LTRjZmItYTM5Ny0zYzcxOWUxOTlhZGMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.tIIZRloyslhr1Z9Z3E4wZA39NQdtvBf5gRvavUxc5js'),
               )
             ],
           )
